b5e76b9c0841a14f433c39dfbeaaa6fc8371239b
Abhishek140298 <abhishekyadavfeb1498@gmail.com>
2025-03-10 22:26:51 +0530

    Type script added

--- ReactJs/React_InterviewComponents/src/components/todo.tsx
+++ ReactJs/React_InterviewComponents/src/components/todo.tsx
@@ -1,54 +1,53 @@
 import React, { useState } from "react";
 
-const Todo = () => {
-  const [tasks, setTask] = useState([]);
-  const [taskValue, setTaskValue] = useState("");
+interface Task {
+  key: number;
+  task: string;
+}
+
+const Todo: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [taskValue, setTaskValue] = useState<string>("");
 
   const taskAdd = () => {
-    setTask([...tasks, { key: tasks.length, task: taskValue }]);
-    setTaskValue(prev=>'')
+    if (taskValue.trim()) {
+      setTasks([...tasks, { key: tasks.length, task: taskValue }]);
+      setTaskValue("");
+    }
   };
 
-  const taskHandler = (event) => {
+  const taskHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTaskValue(event.target.value);
   };
 
-  const deleteTask = (task) => {
+  const deleteTask = (task: Task) => {
     const removedTaskList = tasks.filter((value) => value.key !== task.key);
-    setTask(removedTaskList);
+    setTasks(removedTaskList);
   };
- 
 
   return (
     <div>
       <h1>TODO</h1>
       <div>
-        {" "}
-        <input className="task_name" type="text" onChange={taskHandler} value={taskValue} />
+        <input
+          className="task_name"
+          type="text"
+          onChange={taskHandler}
+          value={taskValue}
+        />
         <button className="add_task" onClick={taskAdd}>
           Add
         </button>
       </div>
-
-      {tasks.length > 0
-        && tasks.map((task) => {
-            return (
-              <div key={task.key}>
-                <div>{task.task}</div>
-                <button onClick={() => deleteTask(task)}>Delete</button>
-              {/*<button onClick={() => deleteTask(task)}>Edit</button> */}
-              </div>
-            );
-          })
-       }
+      {tasks.length > 0 &&
+        tasks.map((task) => (
+          <div key={task.key}>
+            <div>{task.task}</div>
+            <button onClick={() => deleteTask(task)}>Delete</button>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Todo;
-
-//Input Box
-//Create Task
-//List of todo task
-//Container to show todo task
-//Delete task
